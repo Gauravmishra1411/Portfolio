27f6dd0f1f61440fb774a2f1dba901c2b911424a
Gauravmishra1411 <iamgauravdbg@gmail.com>
2024-08-06 10:52:02 +0530

    update home section

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,32 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Manohar</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            I am a result-oriented and self-motivated MCA student looking <br />{" "}
+            for a challenging career where I can implement my coding knowledge
+          </p>
+        </div>
+        <button className="btn btn-primary">
+          <a
+          target="_blank"
+          href="https://drive.google.com/file/d/1-1a9AA2ECYIbRSq5OsoyeUTR7i8iM2DC/view?usp=sharing"
+             className="github-btn btn-h"
+          >
+            Resume
+          </a>
+        </button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.jpeg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
